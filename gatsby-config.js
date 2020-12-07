6d5525cecdf275a06a6cf329dacfdcfdeacbb621
Noah Anderson <nanderson815@gmail.com>
2020-12-07 18:58:25 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,33 @@
 module.exports = {
   plugins: [
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-source-contentful',
       options: {
-        accessToken: "mA5OzKSjQ_8ovyljllfnXW9_CSPyGh3Co0Xm6E5EKQY",
-        spaceId: "pwq99q74u76t",
+        accessToken: 'mA5OzKSjQ_8ovyljllfnXW9_CSPyGh3Co0Xm6E5EKQY',
+        spaceId: 'pwq99q74u76t',
       },
     },
-    "gatsby-plugin-sass",
+    'gatsby-plugin-sass',
+    'gatsby-plugin-material-ui',
+    'gatsby-plugin-eslint',
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: "G-C9C19764FN",
+        trackingId: 'G-C9C19764FN',
       },
     },
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-offline",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: '/src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
 };
