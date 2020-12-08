b401389085503588115bc0378ecbba39aec11a4a
Noah Anderson <nanderson815@gmail.com>
2020-12-07 20:37:35 -0500

    setup done

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import Container from '@material-ui/core/Container'
 
 export default function Main({ children }) {
-  return <Container maxWidth="xlgit ">{children}</Container>
+  return <Container maxWidth="xl">{children}</Container>
 }
---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../containers/Main'
+
+const NotFoundPage = () => (
+  <Main>
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </Main>
+)
+
+export default NotFoundPage
