6a75ff1851a6dd3f4c18c51b2f01c44a105b3a7d
Noah Anderson <nanderson815@gmail.com>
2020-12-07 20:14:17 -0500

    finished setting up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,13 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   plugins: [
     {
       resolve: 'gatsby-source-contentful',
       options: {
-        accessToken: 'mA5OzKSjQ_8ovyljllfnXW9_CSPyGh3Co0Xm6E5EKQY',
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
         spaceId: 'pwq99q74u76t',
       },
     },
@@ -29,5 +33,18 @@ module.exports = {
       },
       __key: 'images',
     },
+    {
+      resolve: 'gatsby-plugin-firebase',
+      options: {
+        credentials: {
+          apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+          authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+          projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+          storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.GATSBY_FIREBASE_APP_ID,
+        },
+      },
+    },
   ],
-};
+}
