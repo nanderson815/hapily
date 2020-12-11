ab79727a690c9c7aeb5a3e1b94a476f74803126a
Noah Anderson <nanderson815@gmail.com>
2020-12-10 20:18:23 -0500

    done with setup

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,18 @@
 import { Typography } from '@material-ui/core'
 import React from 'react'
-// import { useCollection } from 'react-firebase-hooks/firestore'
-// import firebase from 'gatsby-plugin-firebase'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import firebase from 'gatsby-plugin-firebase'
 
 import Main from '../containers/Main'
 
 // markup
 const IndexPage = () => {
-  // const [value, loading, error] = useCollection(
-  //   firebase.firestore().collection('partners')
-  // )
-  // console.log('val:', value)
-  // console.log('loading:', loading)
-  // console.log(error)
+  const [value, loading, error] = useCollection(
+    firebase.firestore().collection('partners')
+  )
+  console.log('val:', value.docs.data)
+  console.log('loading:', loading)
+  console.log(error)
 
   return (
     <Main>
